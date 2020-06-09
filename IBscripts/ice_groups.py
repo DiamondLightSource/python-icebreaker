df93ba2eff18527d69d8ef0dd42c0f224c7508a2
Donovan <lexi@Lexi>
2020-06-09 14:37:13 +0100

    reformatted, cleaned script

--- IBscripts/ice_groups.py
+++ IBscripts/ice_groups.py
@@ -1,10 +1,8 @@
 import collections
 import sys
 import os
-# import pandas as pd
 import mrcfile
 import numpy as np
-from itertools import groupby
 
 import gemmi
 import json
@@ -12,91 +10,45 @@ sys.path.insert(0, "/home/lexi/Documents/Diamond/ICEBREAKER/")
 import star_appender
 
 
-# pd.set_option("display.max_colwidth", 10000)
+def main(starfile, mic_path):
 
-path_to_star = sys.argv[1]
-path_to_micrographs = sys.argv[2]
+    in_doc = gemmi.cif.read_file(starfile)
+    data_as_dict = json.loads(in_doc.as_json())['particles']
 
-starfile = path_to_star
-full_path = path_to_micrographs
+    micrographs_used = data_as_dict['_rlnmicrographname']
+    micrographs_unique = list(set(micrographs_used))
+    num_mics = len(micrographs_unique)
 
-'''
-last = 99999999
-header = ''
-rC = 0
-with open(starfile, 'r') as f:
-    for num, line in enumerate(f, 1):
-        if '_rln' in line:
-            last = num
+    mic_coord = collections.OrderedDict()
+    for mic in micrographs_unique:
+        mic_coord[mic] = [i for i, e in enumerate(micrographs_used)
+                          if e == mic]
+    print(mic_coord)
 
-        elif(num > last):
-            break
-        header += line
-        if 'MicrographName' in line:
-            micro_name = [int(''.join(s)) for is_digit,
-                          s in groupby(line, str.isdigit) if is_digit]
+    ice_groups = []
+    for k in range(num_mics):
+        print(f'{k} / {num_mics}')
+        mic = micrographs_unique[k]
+        im_path = os.path.join(mic_path, os.path.split(
+                mic[:-4])[-1] + '_grouped.mrc')
 
-        if 'CoordinateX' in line:
-            coord_X = [int(''.join(s)) for is_digit,
-                       s in groupby(line, str.isdigit) if is_digit]
+        with mrcfile.open(im_path, 'r+', permissive=True) as mrc:
+            micro_now = mrc.data
 
-        if 'CoordinateY' in line:
-            coord_Y = [int(''.join(s)) for is_digit,
-                       s in groupby(line, str.isdigit) if is_digit]
+        for part_ind in mic_coord[mic]:
+            x1 = int(np.floor(data_as_dict['_rlncoordinatex'][part_ind]))
+            y1 = int(np.floor(data_as_dict['_rlncoordinatey'][part_ind]))
+            if micro_now is not None:
+                ice_groups.append(int(micro_now[y1][x1]*10000))
+            else:
+                ice_groups.append(-1)
 
-        if '_rln' in line:
-            rC += 1
+    star_appender.update_star(starfile, ice_groups)
 
-d1 = pd.read_csv(starfile, sep='\s+', skiprows=last, header=None)
+    return True
 
-micrographs_used = (d1[:][micro_name[0]-1].unique())
-'''
 
-in_doc = gemmi.cif.read_file(starfile)
-data_as_dict = json.loads(in_doc.as_json())['particles']
-
-micrographs_used = data_as_dict['_rlnmicrographname']
-micrographs_unique = list(set(micrographs_used))
-num_mics = len(micrographs_unique)
-
-# mic_coord = collections.OrderedDict()
-# for i in range(num_mics):
-#     mic_coord[str(micrographs_used[i])] = (d1.loc[d1[micro_name[0]-1] == str(
-#             micrographs_used[i])].index[:]).tolist()
-
-# Get all indices of particles from given micrograph
-mic_coord = collections.OrderedDict()
-for mic in micrographs_unique:
-    mic_coord[mic] = [i for i, e in enumerate(micrographs_used) if e == mic]
-print(mic_coord)
-
-ice_groups = []
-for k in range(num_mics):
-    print(f'{k} / {num_mics}')
-    mic = micrographs_unique[k]
-    im_path = os.path.join(full_path, os.path.split(
-            mic[:-4])[-1] + '_grouped.mrc')
-
-    with mrcfile.open(im_path, 'r+', permissive=True) as mrc:  #! change to right path and patch size
-        micro_now = mrc.data
-
-    for part_ind in mic_coord[mic]:
-        x1 = int(np.floor(data_as_dict['_rlncoordinatex'][part_ind]))
-        y1 = int(np.floor(data_as_dict['_rlncoordinatey'][part_ind]))
-        if micro_now is not None:
-            ice_groups.append(int(micro_now[y1][x1]*10000))
-        else:
-            ice_groups.append(-1)
-
-star_appender.update_star(starfile, ice_groups)
-
-'''
-header += '_rlnHelicalTubeID #'+str(rC+1)+'\n'  # change number
-d2 = pd.DataFrame({'_rlnHelicalTubeID': ice_groups})   # Does this make sense?? - assuming order is correct...
-
-result = pd.concat([d1, d2], axis=1, sort=False)
-result = result.to_string(header=None, index=None)
-with open(starfile[:-5] + '_icegroups_from_original_mean_scaled.star', 'w') as fd:
-    fd.write(header)
-    fd.write(result)
-'''
+if __name__ == '__main__':
+    starfile = sys.argv[1]
+    mic_path = sys.argv[2]
+    main(starfile, mic_path)
