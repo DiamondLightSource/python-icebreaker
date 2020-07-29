5d65a025cc8bba1d883a9dc5531aa8860d2bf020
m-olek <mjo508@york.ac.uk>
2020-07-29 17:43:10 +0100

    updated equalize - single CPU

--- IBscripts/icebreaker_equalize.py
+++ IBscripts/icebreaker_equalize.py
@@ -4,14 +4,14 @@ Output is group of equalized images.
 """
 import sys
 import mrcfile
-import cv2
+# import cv2
 import numpy as np
 import os
 
-import filter_designer as fd
-import window_mean as wm
-import local_mask as lm
-import KNN_segmenter as KNN_seg
+# import filter_designer as fd
+# import window_mean as wm
+# import local_mask as lm
+# import KNN_segmenter as KNN_seg
 
 
 def load_img(img_path):
@@ -75,8 +75,8 @@ def main(indir):
         y_patches = 40
         num_of_segments = 32
 
-        final_image = equalize_im(img, x_patches, y_patches, num_of_segments)
-        # final_image = img  # !!!! TESTING
+        # final_image = equalize_im(img, x_patches, y_patches, num_of_segments)
+        final_image = img  # !!!! TESTING
 
         #with mrcfile.new((path1+str(filename[:-4]) +'_'+str(x_patches)+'x'+str(y_patches)+'x'+str(num_of_segments)+'flattened'+'.mrc'), overwrite=True) as out_image:    # Make fstring
         with mrcfile.new(os.path.join(path1, filename[:-4] + f'_{outdir}.mrc'), overwrite=True) as out_image:    # Make fstring
