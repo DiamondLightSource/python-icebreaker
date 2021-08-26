d1ffc56aae38c7949d7d9b10b4acfa9c6593336f
m-olek <mjo508@york.ac.uk>
2021-08-26 15:46:31 +0100

    skip if NaN

--- IBscripts/five_figures.py
+++ IBscripts/five_figures.py
@@ -9,7 +9,7 @@ import seaborn as sns
 
 orig_stdout = sys.stdout
 indir = sys.argv[1]
-f = open(indir+'five_figs.csv','w')
+f = open(indir+'five_figs_test.csv','w')
 sys.stdout = f
 files = []
 for filename in os.listdir(indir):
@@ -27,7 +27,7 @@ print('path','min','q1','q2=median','q3','max')
 for img_path in files:
 	with mrcfile.open(img_path, 'r', permissive=True) as mrc:
 		img = mrc.data
-		if(np.isnan(img)):
+		if(np.isnan(np.sum(img))):
 			continue
 		else:
 			print(img_path[:-4],int(np.min(img)*r),int(np.quantile(img,0.25)*r),int(np.median(img)*r),int(np.quantile(img,0.75)*r),int(np.max(img)*r))
