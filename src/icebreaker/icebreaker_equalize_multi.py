372d9a363e72eaa70070b1b4baa45a32ec512a25
Anna Horstmann <anna.horstmann@diamond.ac.uk>
2021-10-05 14:42:03 +0100

    Add batch_size definition.

--- src/icebreaker/icebreaker_equalize_multi.py
+++ src/icebreaker/icebreaker_equalize_multi.py
@@ -121,4 +121,5 @@ def main(indir, cpus):
 
 if __name__ == "__main__":
     indir = sys.argv[1]
+    batch_size = sys.argv[2]
     main(indir, batch_size)
--- src/icebreaker/icebreaker_icegroups_multi.py
+++ src/icebreaker/icebreaker_icegroups_multi.py
@@ -112,4 +112,5 @@ def main(indir, cpus):
 
 if __name__ == "__main__":
     indir = sys.argv[1]
+    batch_size = sys.argv[2]
     main(indir, batch_size)
