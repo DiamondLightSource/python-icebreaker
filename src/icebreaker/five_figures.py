580da35ca8e5ebd5d7f7171b079b5fdb0179a3bf
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2021-10-27 10:11:48 +0100

    Correct paths to mrc files within `five_figures` script (#7)
    The paths returned by the `glob` generator will already contain `indir` so it doesn't need to be added onto the front. Use full image paths

--- src/icebreaker/five_figures.py
+++ src/icebreaker/five_figures.py
@@ -1,24 +1,27 @@
 import sys
 import numpy as np
+
 # import matplotlib.pyplot as plt
 # from mpl_toolkits.mplot3d import Axes3D
 import mrcfile
+
 # import cv2
 # import os
 # import seaborn as sns
 from pathlib import Path
 
 
-def main(mic_path):
-    indir = mic_path
-    with open('five_figs_test.csv', 'w') as f:
-        files = [str(Path(indir / filename)) for filename in Path(indir).glob('**/*')
-                 if filename.suffix == '.mrc']
+def main(grouped_mic_dir):
+    with open("five_figs_test.csv", "w") as f:
+        files = [
+            str(filename)
+            for filename in Path(grouped_mic_dir).glob("**/*.mrc")
+        ]
         r = 10000
 
         f.write("path,min,q1,q2=median,q3,max\n")
         for img_path in sorted(files):
-            with mrcfile.open(img_path, 'r', permissive=True) as mrc:
+            with mrcfile.open(img_path, "r", permissive=True) as mrc:
                 img = mrc.data
                 if not np.isnan(np.sum(img)):
                     path = img_path[:-4]
@@ -30,6 +33,6 @@ def main(mic_path):
                     f.write(f"{path},{min},{q1},{median},{q3},{max}\n")
 
 
-if __name__ == '__main__':
-    mic_path = sys.argv[1]
-    main(mic_path)
+if __name__ == "__main__":
+    grouped_mic_dir = sys.argv[1]
+    main(grouped_mic_dir)
