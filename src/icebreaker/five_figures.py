8542c56288629de8bf174ce127cb54df55a37920
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2021-11-12 10:21:11 +0000

    Parallelise the five_figures script (#8)
    * Use a multiprocessing pool to parallelise the five_figures script
    
    * Rename variable to pass static code analysis

--- src/icebreaker/five_figures.py
+++ src/icebreaker/five_figures.py
@@ -1,36 +1,42 @@
 import sys
 import numpy as np
 
-# import matplotlib.pyplot as plt
-# from mpl_toolkits.mplot3d import Axes3D
 import mrcfile
 
-# import cv2
-# import os
-# import seaborn as sns
+from multiprocessing import Pool, Lock, Manager
 from pathlib import Path
 
 
-def main(grouped_mic_dir):
-    with open("five_figs_test.csv", "w") as f:
-        files = [
-            str(filename)
-            for filename in Path(grouped_mic_dir).glob("**/*.mrc")
-        ]
-        r = 10000
+def _process_mrc(img_path: str, csv_lines: list, lock: Lock, r: int) -> None:
+    with mrcfile.open(img_path, "r", permissive=True) as mrc:
+        img = mrc.data
+        if not np.isnan(np.sum(img)):
+            path = img_path[:-4]
+            min = int(np.min(img) * r)
+            q1 = int(np.quantile(img, 0.25) * r)
+            median = int(np.median(img) * r)
+            q3 = int(np.quantile(img, 0.75) * r)
+            max = int(np.max(img) * r)
+    lock.acquire()
+    csv_lines.append(f"{path},{min},{q1},{median},{q3},{max}\n")
+    lock.release()
+    return None
 
+
+def main(grouped_mic_dir: str, cpus: int = 1) -> None:
+    manager = Manager()
+    lock = manager.Lock()
+    files = [str(filename) for filename in Path(grouped_mic_dir).glob("**/*.mrc")]
+    r = 10000
+
+    csv_lines = manager.list()
+    with Pool(cpus) as p:
+        p.starmap(_process_mrc, [(fl, csv_lines, lock, r) for fl in sorted(files)])
+    with open("five_figs_test.csv", "w") as f:
         f.write("path,min,q1,q2=median,q3,max\n")
-        for img_path in sorted(files):
-            with mrcfile.open(img_path, "r", permissive=True) as mrc:
-                img = mrc.data
-                if not np.isnan(np.sum(img)):
-                    path = img_path[:-4]
-                    min = int(np.min(img) * r)
-                    q1 = int(np.quantile(img, 0.25) * r)
-                    median = int(np.median(img) * r)
-                    q3 = int(np.quantile(img, 0.75) * r)
-                    max = int(np.max(img) * r)
-                    f.write(f"{path},{min},{q1},{median},{q3},{max}\n")
+        for line in csv_lines:
+            f.write(line)
+    return None
 
 
 if __name__ == "__main__":
