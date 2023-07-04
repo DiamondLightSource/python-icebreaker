8f7b6dd464b1c640b0a7a4878f905ed3ef146ea7
m-olek <mjo508@york.ac.uk>
2023-07-04 16:55:37 +0100

    mrc stacks added as possible input

--- src/icebreaker/icebreaker_equalize_multi.py
+++ src/icebreaker/icebreaker_equalize_multi.py
@@ -14,6 +14,8 @@ from icebreaker import filter_designer as fd
 from icebreaker import window_mean as wm
 from icebreaker import local_mask as lm
 from icebreaker import KNN_segmenter as KNN_seg
+
+
 import time
 
 from sklearn.cluster import KMeans
@@ -29,6 +31,7 @@ def multigroup(filelist_full):
     # for filename in filelist:
     img = load_img(filelist_full)  # (os.path.join(indir, filename))
     splitpath = os.path.split(filelist_full)
+
     # Config params
     x_patches = 40
     y_patches = 40
@@ -54,32 +57,51 @@ def recreate_image(codebook, labels, w, h):
 
 def equalize_im(img, x_patches, y_patches, num_of_segments):
 
-    lowpass = img
-    rolled = wm.window(lowpass, x_patches, y_patches)
-    rolled_resized = cv2.resize(rolled, (185, 190), interpolation=cv2.INTER_NEAREST)
-    rolled_resized = cv2.GaussianBlur(rolled_resized, (5, 5), 0)
-    a = np.reshape(rolled_resized, (185*190, -1))
+    print(img.shape)
+    if len(img.shape)==3:
+        stack = np.zeros((img.shape[0],img.shape[1],img.shape[2]),dtype='int16')
+        stack_size = img.shape[0]
+    else:
+        stack = np.zeros((img.shape[0],img.shape[1]),dtype='int16')
+        stack_size = 1
+
+    print(stack_size)	
+
+    for j in range(stack_size):
+        if stack_size > 1:
+            lowpass = img[j,:,:]
+        else: lowpass = img 
+    #tmp = img
+        rolled = wm.window(lowpass, x_patches, y_patches)
+        rolled_resized = cv2.resize(rolled, (185, 190), interpolation=cv2.INTER_NEAREST)
+        rolled_resized = cv2.GaussianBlur(rolled_resized, (5, 5), 0)
+        a = np.reshape(rolled_resized, (185*190, -1))
 
     ##b = cudf.DataFrame(a)
     ##kmeans_float = KMeans(n_clusters=num_of_segments,max_iter=300,random_state=0,output_type='numpy')
-    kmeans_float = KMeans(n_clusters=num_of_segments,init='k-means++',random_state=0,max_iter = 300)
-    labels=kmeans_float.fit_predict(a)
+        kmeans_float = KMeans(n_clusters=num_of_segments,init='k-means++',random_state=0,max_iter = 300)
+        labels=kmeans_float.fit_predict(a)
 
-    KNNsegmented=recreate_image(kmeans_float.cluster_centers_, labels, 190, 185)
+        KNNsegmented=recreate_image(kmeans_float.cluster_centers_, labels, 190, 185)
 
-    upscaled_region = cv2.resize(KNNsegmented, (lowpass.shape[1], lowpass.shape[0]), interpolation=cv2.INTER_AREA)
+        upscaled_region = cv2.resize(KNNsegmented, (lowpass.shape[1], lowpass.shape[0]), interpolation=cv2.INTER_AREA)
 
-    regions_vals = np.unique(KNNsegmented)
-    averaged_loc = np.zeros(
-        (lowpass.shape[0], lowpass.shape[1], num_of_segments), np.uint8
+        regions_vals = np.unique(KNNsegmented)
+        averaged_loc = np.zeros(
+            (lowpass.shape[0], lowpass.shape[1], num_of_segments), np.uint8
     )
-    res = np.zeros((lowpass.shape[0], lowpass.shape[1]), np.uint8)
-    for i in range(len(regions_vals)):
-        averaged_loc[:, :, i] = lm.local_mask(lowpass, KNNsegmented, regions_vals[i])
-        res[:, :] += averaged_loc[:, :, i]
+        res = np.zeros((lowpass.shape[0], lowpass.shape[1]), np.uint8)
+        for i in range(len(regions_vals)):
+            averaged_loc[:, :, i] = lm.local_mask(lowpass, KNNsegmented, regions_vals[i])
+            res[:, :] += averaged_loc[:, :, i]
     
-    final_image = res  # cv2.GaussianBlur(res, (45, 45), 0)
-    return final_image
+        final_image = res  # cv2.GaussianBlur(res, (45, 45), 0)
+        
+        if stack_size > 1:
+            stack[j,:,:] = final_image
+        else: stack = final_image
+
+    return stack#final_image
 
 
 def main(indir, cpus):
