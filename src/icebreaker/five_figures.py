df54c49ac3d616dd7e3a64cea2b6192ec37de312
stephen-riggs <stephen.riggs@diamond.ac.uk>
2023-08-30 16:36:30 +0100

    Don't remove suffix from files in summary

--- src/icebreaker/five_figures.py
+++ src/icebreaker/five_figures.py
@@ -10,13 +10,12 @@ def single_mic_5fig(mic_name: str, r: int = 10000) -> str:
     with mrcfile.open(mic_name, "r", permissive=True) as mrc:
         img = mrc.data
         if not np.isnan(np.sum(img)):
-            path = mic_name[:-4]
             min = int(np.min(img) * r)
             q1 = int(np.quantile(img, 0.25) * r)
             median = int(np.median(img) * r)
             q3 = int(np.quantile(img, 0.75) * r)
             max = int(np.max(img) * r)
-            csv_lines = f"{path},{min},{q1},{median},{q3},{max}"
+            csv_lines = f"{mic_name},{min},{q1},{median},{q3},{max}"
         else:
             csv_lines = ""
     return csv_lines
